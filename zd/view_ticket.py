14aa6d74ed583b7e755e49f40d8635bd0a2b3908
Rohan Gosavi <64428362+rohangosavi01@users.noreply.github.com>
2021-11-29 04:11:02 -0600

    Update view_ticket.py

--- zd/view_ticket.py
+++ zd/view_ticket.py
@@ -8,7 +8,10 @@ def view_ticket_info(dictionary, number):
     ticket_description = ticket_information['description']
     ticket_requester = ticket_information['requester_id']
     ticket_priority = ticket_information['priority']
-    custom_dictionary = {'Ticket Priority':ticket_priority, 'Subject': ticket_subject, 'Description':ticket_description, 'Status':ticket_status, 'Requester ID':ticket_requester, 'Created On':ticket_created}
+    ticket_organization_id = ticket_information['organization_id']
+    ticket_due_at = ticket_information['due_at']
+    custom_dictionary = {'Ticket Priority': ticket_priority, 'Created On': ticket_created, 'Subject': ticket_subject, 'Description': ticket_description,
+                         'Status': ticket_status, 'Requester ID': ticket_requester, 'Organization ID': ticket_organization_id, 'Due at': ticket_due_at}
     return custom_dictionary
 
     
